2fa58c6d09ed5671f860e899181bae7f26426fdb
Bodomrus70 <Bodomrus70@mail.ru>
2023-02-28 22:54:28 +0700

    Modes_1

---
+++ Module_17/Modesgeng/SalaryCalculateMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_17_Patterns.Modes
+{
+    public class SalaryCalculateMode : ICalculateMode
+    {
+        public void Calculate(Account account)
+        {
+            // расчет процентной ставки зарплатного аккаунта по правилам банка
+            account.Interest = account.Balance * 0.5;
+        }
+    }
+}
